722dcb46df033a81f283f9198ca1478db1a43b4b
Dwaipayan Sarkar <81685946+Dwai649@users.noreply.github.com>
2026-04-04 16:33:55 +0530

    changes to network iistener interface

--- retail_app/server.js
+++ retail_app/server.js
@@ -20,6 +20,6 @@ app.get('/', (req, res) => {
 
 // Azure Web App listens on process.env.PORT
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Retail app running on port ${PORT}`);
 });
